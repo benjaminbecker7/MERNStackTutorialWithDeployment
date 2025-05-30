ecc19cd016d6bc00fe899f8a4e1685254aa82779
benenjoyscoding <ben@flagship.shop>
2025-05-30 16:25:19 -0700

    build and deploy

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
-import express from "express";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.route.js";
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+import productRoutes from './routes/product.route.js'
+import path from 'path'
 
-dotenv.config();
-const PORT = process.env.SERVER_PORT ?? 5001;
+dotenv.config()
+const PORT = process.env.SERVER_PORT ?? 5001
 
-const app = express();
+const app = express()
 
-app.use(express.json()); // allows us to accept JSON bodies in req.body
+const __dirname = path.resolve()
 
-app.use("/api/products", productRoutes);
+app.use(express.json()) // allows us to accept JSON bodies in req.body
+
+app.use('/api/products', productRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server started at http://localhost:${PORT}`);
-});
+  connectDB()
+  console.log(`Server started at http://localhost:${PORT}`)
+})
